611c430388c80fb0ee6bc2ab69993e85c94f7565
Brian Woodard <bwoodard@globalcashcard.com>
2015-06-29 10:58:00 -0700

    Beta Module Complete

--- signacert/manifests/defaults.pp
+++
@@ -1,8 +0,0 @@
-class foo::defualts {
-  case $osfamily {
-    'Centos' : {
-      $package_name = 'bleh'
-      $service_name = 'bleh'
-      $config_path  = '/etc/blah/bleh.conf'
-      $log_fil      = '/etc/bleh/blah/bleet.log'
-      $storgage+
--- signacert/manifests/init.pp
+++ signacert/manifests/init.pp
@@ -36,9 +36,42 @@
 # Copyright 2015 Your name here, unless otherwise noted.
 #
 class signacert {
-  file { '/etc/signacertexample.conf' :
-    source => 'puppet:///modules/signacert/signacertexample.conf',
-      owner => 'root',
-      group => 'root',
-      mode  => '640',
-}
+  user { "signacert":
+    ensure     => 'present',
+    gid        => 'signacert',
+    groups     => ['wheel'],
+    shell      => '/bin/bash',
+    home       => '/home/signacert',
+    managehome => true,
+  }
+  ssh_authorized_key { 'sigancert':
+    ensure => 'present',
+    type   => 'ssh-rsa',
+    key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAy5WxN4CcVdhLNkIU4eKsJJzyyPmXmnD+tAa4gkigLws/G0/jF8yNu8kZZlH9D44Z06Vf/7smX6GEb28IjF7892cfMszK19Yy4cH3gHD59wXUEPv/0ELeKJv2Efiusi55bFopdH8bgpvlgYHq2bto7IGLiyDH5Pad9QRiDH9i9B9fmVAYYITdb8x4ZlXBo3mfEJ4gfwo38a9ZBbSUO56kfu/iMPgvRZKepOsV1Ih3pkpnd4BdK/GDs6VSL3vbVly+U21Qg8zL3K5gJptTD2VudXLp95JdsQsKEzlhgUybAHsDwIZj6LZe5JXvvqWjwFZOlQlfHF42tN1BNd8kaWX5yQ== signacert@sd-staging-cardholder1.inside.globalcashcard.com',
+   user    => 'signacert',
+  }
+  file { '/tmp/signacert_oval-5_8_0_2-rhel6_x64.tar.gz':
+    ensure  => 'present',
+    source  => 'puppet:///modules/signacert/signacert_oval-5_8_0_2-rhel6_x64.tar.gz',
+    mode    => '0755',
+    owner   => 'signacert',
+    group   => 'wheel',
+  }
+  exec { 'signa_oval_install':
+    command   => 'tar -xzvf /tmp/signacert_oval-5_8_0_2-rhel6_x64.tar.gz -C /opt; rm -rf /tmp/signacert_oval-5_8_0_2-rhel6_x64.tar.gz',
+    creates   => '/opt/signacert_oval',
+    logoutput => true,
+  }
+  file { '/tmp/signacert_tools-5_8_0_2-linux_x64.tar.gz':
+    ensure => 'present',
+    source => 'puppet///modules/signacert/signacert_tools-5_8_0_2-linux_x64.tar.gz',
+    mode   => '0755',
+    owner  => 'signacert',
+    group  => 'wheel',
+  }
+  exec { 'signa_tools_install'
+    command   => 'tar -xzvf /tmp/signacert_tools-5_8_0_2-linux_x64.tar.gz -C /opt; rm -rf /tmp/signacert_tools-5_8_0_2-linux_x64.tar.gz',
+    creates   => '/opt/signacert/jre/',
+    logoutput => true,
+  }
+    
