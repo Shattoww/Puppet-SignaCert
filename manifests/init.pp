7aadd601e96d3c5f9b07db18477bf71a59ab8ff2
Cesar Covarrubias <ccovarrubias@globalcashcard.com>
2015-10-07 14:10:53 -0700

    Fixed group for signacert user. Fixed perms on signacert dir.

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,23 +14,26 @@
 class signacert {
   user { 'signacert':
     ensure     => 'present',
-    groups     => ['signacert'],
+    groups     => ['signacert','wheel'],
     shell      => '/bin/bash',
     home       => '/home/signacert',
     managehome => true,
-	require    => Group['signacert'],
+  require      => Group['signacert'],
   }
 
-  group { "signacert":
-    ensure => "present",
+  group { 'signacert':
+    ensure => 'present',
   }
+
   ssh_authorized_key { 'sigancert':
     ensure => 'present',
     type   => 'ssh-rsa',
     key    => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAy5WxN4CcVdhLNkIU4eKsJJzyyPmXmnD+tAa4gkigLws/G0/jF8yNu8kZZlH9D44Z06Vf/7smX6GEb28IjF7892cfMszK19Yy4cH3gHD59wXUEPv/0ELeKJv2Efiusi55bFopdH8bgpvlgYHq2bto7IGLiyDH5Pad9QRiDH9i9B9fmVAYYITdb8x4ZlXBo3mfEJ4gfwo38a9ZBbSUO56kfu/iMPgvRZKepOsV1Ih3pkpnd4BdK/GDs6VSL3vbVly+U21Qg8zL3K5gJptTD2VudXLp95JdsQsKEzlhgUybAHsDwIZj6LZe5JXvvqWjwFZOlQlfHF42tN1BNd8kaWX5yQ==',
     user   => 'signacert',
   }
+
   if $::signacertinstalled == false {
+
     file { '/tmp/signacert_oval-5_8_0_2-rhel6_x64.tar.gz':
       ensure => 'present',
       source => 'puppet:///modules/signacert/signacert_oval-5_8_0_2-rhel6_x64.tar.gz',
@@ -38,13 +41,15 @@ class signacert {
       owner  => 'signacert',
       group  => 'wheel',
     }
+
     exec { 'signa_oval_install':
       command   => 'tar -xzvf /tmp/signacert_oval-5_8_0_2-rhel6_x64.tar.gz -C /opt; rm -rf /tmp/signacert_oval-5_8_0_2-rhel6_x64.tar.gz',
-      path      => "/bin",
+      path      => '/bin',
       require   => File['/tmp/signacert_oval-5_8_0_2-rhel6_x64.tar.gz'],
       creates   => '/opt/signacert/signaclient/',
       logoutput => true,
     }
+
     file { '/tmp/signacert_tools-5_8_0_2-linux_x64.tar.gz':
       ensure => 'present',
       source => 'puppet:///modules/signacert/signacert_tools-5_8_0_2-linux_x64.tar.gz',
@@ -52,12 +57,22 @@ class signacert {
       owner  => 'signacert',
       group  => 'wheel',
     }
+
     exec { 'signa_tools_install':
       command   => 'tar -xzvf /tmp/signacert_tools-5_8_0_2-linux_x64.tar.gz -C /opt; rm -rf /tmp/signacert_tools-5_8_0_2-linux_x64.tar.gz',
-      path      => "/bin",
+      path      => '/bin',
       require   => File['/tmp/signacert_tools-5_8_0_2-linux_x64.tar.gz'],
       creates   => '/opt/signacert/jre/',
       logoutput => true,
     }
-}
+
+  }
+
+  file { '/opt/signacert':
+    ensure  => present,
+  owner     => 'signacert',
+    group   => 'signacert',
+    recurse => true,
+  }
+
 }
